b709ed96163b0b88bdd21108bf2189507ac81ad1
Nulviete <nulvietz@gmail.com>
2024-03-04 17:31:43 +0100

    asdasdasd

---
+++ assets/IndexPage.a2cad7ee.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.fffa2538.js";import{_ as a,X as o,J as t,K as s,L as n,O as r}from"./index.181651b6.js";const c=o({name:"IndexPage"}),l=r("h1",null,"Hello, Welcome! :)",-1);function _(f,d,p,i,m,x){return t(),s(e,{class:"flex flex-center"},{default:n(()=>[l]),_:1})}var $=a(c,[["render",_]]);export{$ as default};
